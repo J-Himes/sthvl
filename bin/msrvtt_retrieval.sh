70fed14bdc79d9891f208a3e62351fde7c663e32
J-Himes <100195030+J-Himes@users.noreply.github.com>
2022-05-03 17:15:05 -0700

    Add files via upload

---
+++ bin/msrvtt_retrieval.sh
@@ -0,0 +1,26 @@
+# Test runs on 4 GPUs
+
+DATATYPE="msrvtt"
+TRAIN_CSV="data/msrvtt/MSRVTT_train.9k.csv"
+VAL_CSV="data/msrvtt/MSRVTT_JSFUSION_test.csv"
+DATA_PATH="data/msrvtt/MSRVTT_data.json"
+FEATURES_PATH="data/msrvtt/msrvtt_videos_features.pickle"
+INIT_MODEL="weight/univl.pretrained.bin"
+OUTPUT_ROOT="ckpts"
+
+source ~/.bashrc
+conda activate py_univl
+
+python -m torch.distributed.launch --nproc_per_node=4 \
+main_task_retrieval.py \
+--do_train --num_thread_reader=16 \
+--epochs=5 --batch_size=128 \
+--n_display=100 \
+--train_csv ${TRAIN_CSV} \
+--val_csv ${VAL_CSV} \
+--data_path ${DATA_PATH} \
+--features_path ${FEATURES_PATH} \
+--output_dir ${OUTPUT_ROOT}/ckpt_msrvtt_retrieval --bert_model bert-base-uncased \
+--do_lower_case --lr 5e-5 --max_words 48 --max_frames 48 \
+--batch_size_val 64 --visual_num_hidden_layers 6 \
+--datatype ${DATATYPE} --expand_msrvtt_sentences --init_model ${INIT_MODEL}
---
+++ bin/youcook_caption.sh
@@ -0,0 +1,23 @@
+# Test runs on 4 GPUs
+
+TRAIN_CSV="data/youcookii/youcookii_train.csv"
+VAL_CSV="data/youcookii/youcookii_val.csv"
+DATA_PATH="data/youcookii/youcookii_data.no_transcript.pickle"
+FEATURES_PATH="data/youcookii/youcookii_videos_features.pickle"
+INIT_MODEL="weight/univl.pretrained.bin"
+OUTPUT_ROOT="ckpts"
+
+python -m torch.distributed.launch --nproc_per_node=4 \
+main_task_caption.py \
+--do_train --num_thread_reader=4 \
+--epochs=5 --batch_size=16 \
+--n_display=100 \
+--train_csv ${TRAIN_CSV} \
+--val_csv ${VAL_CSV} \
+--data_path ${DATA_PATH} \
+--features_path ${FEATURES_PATH} \
+--output_dir ${OUTPUT_ROOT}/ckpt_youcook_caption --bert_model bert-base-uncased \
+--do_lower_case --lr 3e-5 --max_words 128 --max_frames 96 \
+--batch_size_val 64 --visual_num_hidden_layers 6 \
+--decoder_num_hidden_layers 3 --stage_two \
+--init_model ${INIT_MODEL}
