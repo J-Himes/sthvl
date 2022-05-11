519623fea540a31cd2f93135afaae4153443f0b5
J-Himes <100195030+J-Himes@users.noreply.github.com>
2022-05-10 21:01:18 -0700

    Add files via upload

---
+++ local_setup.sh
@@ -0,0 +1,8 @@
+# Make sure to run this file at the top level directory of your project
+
+source ~/.bashrc
+conda create -n sthvl python=3.6.9 tqdm boto3 requests pandas
+source ~/.bashrc
+conda activate sthvl
+pip install torch==1.10.1
+pip install git+https://github.com/Maluuba/nlg-eval.git@master
