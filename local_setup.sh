8837a7b458031bebc70671d3925068d6d7c507b6
J-Himes <100195030+J-Himes@users.noreply.github.com>
2022-05-10 21:58:42 -0700

    Update local_setup.sh

--- local_setup.sh
+++ local_setup.sh
@@ -4,5 +4,9 @@ source ~/.bashrc
 conda create -n sthvl python=3.6.9 tqdm boto3 requests pandas
 source ~/.bashrc
 conda activate sthvl
+
 pip install torch==1.10.1
 pip install git+https://github.com/Maluuba/nlg-eval.git@master
+
+mkdir -p ./weight
+wget -P ./weight https://github.com/microsoft/UniVL/releases/download/v0/univl.pretrained.bin
