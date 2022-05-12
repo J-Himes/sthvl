601bb0da2158c2e1db5bba9bd5baa93e93d0f2a8
irulenot <abmstarwars@yahoo.com>
2022-05-11 21:59:02 -0700

    Testing if --yes flag works

--- local_setup.sh
+++ local_setup.sh
@@ -1,13 +1,13 @@
 # Make sure to run this file at the top level directory of your project
 
 source ~/.bashrc
-conda create -n sthvl python=3.6.9 tqdm boto3 requests pandas
+conda create -n sthvl python=3.6.9 tqdm boto3 requests pandas --yes
 source ~/.bashrc
 source ~/anaconda3/etc/profile.d/conda.sh
 conda activate sthvl
 
-pip install torch==1.10.1
-pip install git+https://github.com/Maluuba/nlg-eval.git@master
+pip install torch==1.10.1 --yes
+pip install git+https://github.com/Maluuba/nlg-eval.git@master --yes
 
 mkdir -p ./ckpts
 mkdir -p ./weight
