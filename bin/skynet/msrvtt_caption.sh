5ae75ed8b7072245f3718ade0591fcb1d5247836
irulenot <abmstarwars@yahoo.com>
2022-06-02 15:46:57 -0700

    Accidently added outputs. Updated environment.yml and skynet script

--- bin/skynet/msrvtt_caption.sh
+++ bin/skynet/msrvtt_caption.sh
@@ -4,7 +4,6 @@ CONFIG_PATH="conf/skynet"
 CONFIG_NAME="msrvtt_caption"
 
 source ~/.bashrc
-source ~/anaconda3/etc/profile.d/conda.sh
 conda activate sthvl
 
 python -m torch.distributed.run --nproc_per_node=1 main_task_caption.py ${CONFIG_PATH} ${CONFIG_NAME}
