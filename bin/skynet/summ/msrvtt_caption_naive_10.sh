9fa7e91ffcdd24f611d17b53800b71ecc4ee2103
irulenot <abmstarwars@yahoo.com>
2022-06-14 17:39:26 -0700

    Ran locally successfully, prepped skynet experiments and generated features in data dir

---
+++ bin/skynet/summ/msrvtt_caption_naive_10.sh
@@ -0,0 +1,9 @@
+# Test runs on 4 GPUs
+
+CONFIG_PATH="conf/skynet"
+CONFIG_NAME="msrvtt_caption_naive_10"
+
+source ~/.bashrc
+conda activate sthvl
+
+python -m torch.distributed.run --nproc_per_node=1 main_task_caption.py ${CONFIG_PATH} ${CONFIG_NAME}
---
+++ preprocessing/create_msrvtt_summ.py
@@ -0,0 +1,32 @@
+import pickle
+import sys
+import random
+import numpy as np
+from vsumm.vsumm import vsumm
+from vsumm.vsumm_skim import vsumm_skim
+
+def main():
+    features_path = sys.argv[1]
+    summ_type = sys.argv[2]
+    percent = int(sys.argv[3])
+    output_pathfile = sys.argv[4]
+
+    feature_dict = pickle.load(open(features_path, 'rb'))
+    for key in feature_dict:
+        indices = list(range(len(feature_dict[key])))
+        if summ_type == 'naive':
+            frames = len(feature_dict[key])
+            count = int(frames * (percent / 100))
+            selected_frames = random.sample(indices, count)
+        elif summ_type == 'vsumm_key':
+            selected_frames = vsumm(feature_dict[key], 1, percent)
+        elif summ_type == 'vsumm_skim':
+            selected_frames = vsumm_skim(feature_dict[key], 1, percent)
+        deleted_frames = [x for x in indices if x not in selected_frames]
+        feature_dict[key] = np.delete(feature_dict[key], deleted_frames, axis=0)
+
+    with open(output_pathfile, 'wb') as f:
+        pickle.dump(feature_dict, f)
+
+if __name__ == "__main__":
+    main()
