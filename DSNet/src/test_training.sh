a7aaa806fc2bc776e020f4788309c0c41a703b76
J-Himes <100195030+J-Himes@users.noreply.github.com>
2022-06-20 02:14:36 -0700

    Added testing files.
    Pre-trained model evaluation and training testing.

---
+++ DSNet/src/test_training.sh
@@ -0,0 +1,9 @@
+source ~/.bashrc
+source ~/anaconda3/etc/profile.d/conda.sh
+conda activate dsnet
+
+echo
+echo Testing training with anchor-based model.
+echo
+
+python train.py anchor-based --model-dir ../models/ab_basic --splits ../splits/tvsum.yml ../splits/summe.yml
