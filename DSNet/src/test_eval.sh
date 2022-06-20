670bedfcd1f9cc6a2c86ab70c65647a6226d4664
J-Himes <100195030+J-Himes@users.noreply.github.com>
2022-06-20 02:36:28 -0700

    Changed messaging.

--- DSNet/src/test_eval.sh
+++ DSNet/src/test_eval.sh
@@ -4,6 +4,7 @@ conda activate dsnet
 
 echo
 echo Testing evaluation with pretrained models.
+echo
 
 python evaluate.py anchor-based --model-dir ../models/pretrain_ab_basic/ --splits ../splits/tvsum.yml ../splits/summe.yml
 python evaluate.py anchor-free --model-dir ../models/pretrain_af_basic/ --splits ../splits/tvsum.yml ../splits/summe.yml --nms-thresh 0.4
