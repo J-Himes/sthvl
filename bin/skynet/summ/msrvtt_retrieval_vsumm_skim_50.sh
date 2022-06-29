cf2a359f4b3c6da85ab30194f18e9f85d38ce797
irulenot <abmstarwars@yahoo.com>
2022-06-29 16:26:23 -0700

    p2

--- bin/skynet/summ/msrvtt_retrieval_vsumm_skim_50.sh
+++ bin/skynet/summ/msrvtt_retrieval_vsumm_skim_50.sh
@@ -6,4 +6,4 @@ CONFIG_NAME="msrvtt_retrieval_vsumm_skim_50"
 source ~/.bashrc
 conda activate sthvl
 
-python -m torch.distributed.run --nproc_per_node=1 main_task_caption.py ${CONFIG_PATH} ${CONFIG_NAME}
+python -m torch.distributed.run --nproc_per_node=1 main_task_retrieval.py ${CONFIG_PATH} ${CONFIG_NAME}
